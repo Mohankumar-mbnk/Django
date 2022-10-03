4e9d8c4ee6493076e566286f732566022c9acd93
Mohankumar B N <mohankumar.mbnk@gmail.com>
2022-10-03 16:15:15 +0530

    first commit

---
+++ instagram/instagram/__init__.py
@@ -0,0 +1,24 @@
+"""
+instagram 
+reels 
+-------------------------------------------------------------------
+instagram(directory)(project)
+    instagram(package)
+        settings.py  # contains all the settings --> INSTALLED_APPS, DATABASES, MIDDLEWARES
+        urls.py # we will store the urls of a project and we import urls of applicatons
+        asgi.py 
+        wsgi.py
+    manage.py # this is a main file 
+    reels(application)
+        apps.py
+        admin.py
+        urls.py # we will store the urls of an application
+        views.py # we will implement business logic
+        models.py # we will define the database schema
+        tests.py
+
+
+
+
+
+"""
